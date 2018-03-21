ddbd30ff41f62da8dcdbf058b35b19f0846f0477
Allison Tarr <hello@allisontarr.com>
2018-03-21 10:04:25 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Starter Package for new WordPress projects with:
+
+### mu-plugins/
+- In this directory there is a _must-use-plugin_ titled Starter Theme Functionality. This plugin contains useful functions that are project-related rather than theme-related.
+- _Must Use Plugins_ are activated automatically before any other plugins, and cannot be disabled by the user. This is a great way to store important functionality for your project, and ensure it won't be accidentally deactivated or removed.
+- **Resources:**
+    - _[Must Use Plugins](https://codex.wordpress.org/Must_Use_Plugins)_ - WordPress Codex article
+    - Creation of this plugin was inspired by this CSS-Tricks article: _[WordPress Functionality Plugins](https://css-tricks.com/wordpress-functionality-plugins/)_
+    - _[Add Editor Style](https://codex.wordpress.org/Function_Reference/add_editor_style)_ - WordPress Codex article on setting up an `editor-style.css` stylesheet
+
+### plugins/
+
+Useful plugins that I use while developing a new WordPress project.
+
+- Theme Check
+- What Template Am I Using
+- Regenerate Thumbnails
+
+### Project Setup
+
+#### 1. Renaming
+* Search for: `'starter-theme'` and replace with: `'project-name'` (inside single quotations) to capture the text domain
+* Search for: `starter_theme_` and replace with: `project-name_` to capture all the function names
+* Search for: `Text Domain: Starter-Theme` and replace with: `Text Domain: project-name` in style.css
+* Search for (and include the leading space): <code>&nbsp;_starter_theme</code> and replace with: <code>&nbsp;Project Name</code> (with a space before it) to capture DocBlocks
+* Search for: `starter-theme-` and replace with: `project-name-` to capture prefixed handles
+* Search for `project-name.pot` and replace with: `project-name.pot` to capture translation files
+* Search for `starter-theme.dev` and replace with: `project-name.dev` to match your local development URL
+* Edit the theme information in the header of style.scss to meet your needs
+
+#### 2. Setup
+- Gulp is setup for uglifying JavaScript and compiling SASS
+- Download the repository and type `npm install` (or `sudo npm install`) to install the necessary Gulp packages
---
+++ themes/starter-theme/inc/customizer.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * _s Theme Customizer.
+ *
+ * @package Starter Theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function starter_theme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	// Add our social link options.
+	$wp_customize->add_section(
+		'starter_theme_social_links_section',
+		array(
+			'title'       => esc_html__( 'Social Links', 'starter-theme' ),
+			'description' => esc_html__( 'These are the settings for social links. Please limit the number of social links to 5.', 'starter-theme' ),
+			'priority'    => 90,
+		)
+	);
+
+	// Create an array of our social links for ease of setup.
+	$social_networks = array( 'twitter', 'facebook', 'instagram' );
+
+	// Loop through our networks to setup our fields.
+	foreach ( $social_networks as $network ) {
+
+		$wp_customize->add_setting(
+			'starter_theme_' . $network . '_link',
+			array(
+				'default' => '',
+				'sanitize_callback' => 'starter_theme_sanitize_customizer_url',
+			)
+		);
+		$wp_customize->add_control(
+			'starter_theme_' . $network . '_link',
+			array(
+				'label'   => sprintf( esc_html__( '%s Link', 'starter-theme' ), ucwords( $network ) ),
+				'section' => 'starter_theme_social_links_section',
+				'type'    => 'text',
+			)
+		);
+	}
+
+	// Add our Footer Customization section section.
+	$wp_customize->add_section(
+		'starter_theme_footer_section',
+		array(
+			'title'    => esc_html__( 'Footer Customization', 'starter-theme' ),
+			'priority' => 90,
+		)
+	);
+
+	// Add our copyright text field.
+	$wp_customize->add_setting(
+		'starter_theme_copyright_text',
+		array(
+			'default' => '',
+			'sanitize_callback' => 'starter_theme_sanitize_customizer_text',
+		)
+	);
+	$wp_customize->add_control(
+		'starter_theme_copyright_text',
+		array(
+			'label'       => esc_html__( 'Copyright Text', 'starter-theme' ),
+			'description' => esc_html__( 'The copyright text will be displayed beneath the menu in the footer.', 'starter-theme' ),
+			'section'     => 'starter_theme_footer_section',
+			'type'        => 'text',
+			'sanitize'    => 'html',
+		)
+	);
+}
+add_action( 'customize_register', 'starter_theme_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function starter_theme_customize_preview_js() {
+	wp_enqueue_script( 'starter_theme_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'starter_theme_customize_preview_js' );
+
+/**
+ * Sanitize our customizer text inputs.
+ */
+function starter_theme_sanitize_customizer_text( $input ) {
+	return sanitize_text_field( force_balance_tags( $input ) );
+}
+
+/**
+ * Sanitize our customizer URL inputs.
+ */
+function starter_theme_sanitize_customizer_url( $input ) {
+	return esc_url( $input );
+}
